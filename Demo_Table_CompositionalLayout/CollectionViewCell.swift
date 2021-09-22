81b78aace59d002819a97bd90c961c5d039f1291
jackaltsai <hata.s520@gmail.com>
2021-09-22 14:32:44 +0800

    表格(table)

---
+++ Demo_Table_CompositionalLayout/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  Demo_Table_CompositionalLayout
+//
+//  Created by 蔡忠翊 on 2021/9/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+}
--- Demo_Table_CompositionalLayout/ViewController.swift
+++ Demo_Table_CompositionalLayout/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.collectionViewLayout = generateLayout()
     }
-
-
+    
+    // 表格(table)
+    func generateLayout() -> UICollectionViewLayout {
+        
+        let configuration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        return UICollectionViewCompositionalLayout.list(using: configuration)
+        
+    }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(CollectionViewCell.self)", for: indexPath) as! CollectionViewCell
+        cell.imageView.image = UIImage(named: "Image\(indexPath.item + 1)")
+        cell.label.text = "Image\(indexPath.item + 1)"
+        return cell
+    }
+    
+    
+}
